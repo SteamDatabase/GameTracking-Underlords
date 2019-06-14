dd9537f234985d6caf80d2ea6aa3b59460c2af33
Wendy <freeman@steamdb.info>
2019-06-14 16:25:40 +0000

    113 | 13 files | M update.sh, ?? game/dac/pak01_dir/scripts/abilities.json, ?? game/dac/pak01_dir/scripts/botsdefs.json, ?? game/dac/pak01_dir/scripts/botsdifficultylevels.json, ?? game/dac/pak01_dir/scripts/botspersonalities.json, ?? game/dac/pak01_dir/scripts/chat_wheel_lines.json, ?? game/dac/pak01_dir/scripts/events.json, ?? game/dac/pak01_dir/scripts/events/, ?? game/dac/pak01_dir/scripts/items.json, ?? game/dac/pak01_dir/scripts/modifiers.json, ?? game/dac/pak01_dir/scripts/synergies.json, ?? game/dac/pak01_dir/scripts/units.json, ?? game/dac/pak01_dir/scripts/waves.json

--- update.sh
+++ update.sh
@@ -19,4 +19,4 @@ done <   <(find "game/dac/maps/" -type f -name "*.vpk" -print0)
 
 FixUCS2
 
-CreateCommit "$(grep "ClientVersion=" game/dac/steam.inf | grep -o '[0-9\.]*')" "$1"
+CreateCommit "$(grep "^ClientVersion=" game/dac/steam.inf | grep -o '[0-9\.]*')" "$1"
